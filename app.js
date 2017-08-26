10c7ae01991b79c280fee2c18232c125b4cb6f76
Gabriel Lima <gabrielcastroslima@gmail.com>
2017-08-26 20:01:28 -0300

    Adiciona rotas

--- app.js
+++ app.js
@@ -4,17 +4,26 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 
 import activity from './app/routes/activity';
+import task from './app/routes/task';
+import teen from './app/routes/teen';
+import card from './app/routes/card';
+import resonposible from './app/routess/responsible';
+
 
 const app = express();
 
 app.set('port', 8080)
 
 app.use(cors());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 //rotas
 app.route(activity(app))
+app.route(responsible(app))
+app.route(task(app))
+app.route(teen(app))
+app.route(card(app))
 
 export default app;
