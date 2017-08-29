e197c187d3f20e5d7aa3bc41f475f20a7c0d4540
Allison Verdam da Cunha <allisonverdam@gmail.com>
2017-08-28 23:43:35 -0300

    adicionando middleware de 404 e colocando os models no middleware

--- app.js
+++ app.js
@@ -10,23 +10,43 @@ import card from './app/routes/card';
 import responsible from './app/routes/responsible';
 import auth from './app/routes/auth';
 
-import DB from './app/models';
+import models from './app/models';
 
 const app = express();
 
 app.set('port', 8080)
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
-//rotas
+//models on middleware
+app.use((req, res, next) => {
+  req.$models = models
+  next()
+})
+
+app.get('/', (req, res) => {
+  res.render('pages/index', {
+    routes: app._router.stack
+  })
+})
+
+//routes
 app.route(activity(app))
 app.route(responsible(app))
 app.route(task(app))
 app.route(teen(app))
 app.route(card(app))
 app.route(auth(app))
+
+app.use((req, res) => {
+  res.status(404).send('404: Page not Found');
+})
 
 export default app;
